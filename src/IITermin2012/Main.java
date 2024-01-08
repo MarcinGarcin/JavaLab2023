d74ed19b143d9ac14eaa7fea0b915154d7f8e197
Marcin <knucias123@gmail.com>
2024-01-09 00:39:07 +0100

    Final

--- src/IITermin2012/Main.java
+++
@@ -1,8 +0,0 @@
-package IITermin2012;
-
-
-public class Main {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ src/IITermin2012/Zad_1A.java
@@ -0,0 +1,11 @@
+package IITermin2012;
+
+
+import java.util.Random;
+
+public class Zad_1A {
+    public static void main(String[] args) {
+        Random random = new Random();
+        random.nextInt();
+    }
+}
