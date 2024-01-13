c3c092895722d65483e561fecbfd806c9863062a
Marcin <knucias123@gmail.com>
2024-01-13 23:56:08 +0100

    Ended task

--- src/PlikiDanych/Main.java
+++ src/PlikiDanych/Main.java
@@ -6,48 +6,58 @@ public class Main {
 
     public static int przetwarzajPlik(String nazwaPliku, String nazwaTowaru) {
         double rabat = 0.9;
-
         int liczbaSztuk = 0;
+
         try (
                 DataInputStream dis = new DataInputStream(new FileInputStream(nazwaPliku));
                 DataOutputStream dos = new DataOutputStream(new FileOutputStream("temp.txt"))
         ) {
-
-            liczbaSztuk = 0;
-            boolean znaleziono = false;
-
+            int linesAmount = 0;
             while (true) {
                 try {
-                    String towar = dis.readUTF();
-                    double cena = dis.readDouble();
-                    int sztuki = dis.readInt();
-
-                    if (towar.equals(nazwaTowaru) && sztuki >= 100) {
-                        sztuki = (int) (sztuki * rabat);
-                        znaleziono = true;
-                    }
-
-                    dos.writeUTF(towar);
-                    dos.writeDouble(cena);
-                    dos.writeInt(sztuki);
-
-                    if (towar.equals(nazwaTowaru)) {
-                        liczbaSztuk = sztuki;
-                    }
+                    dis.readUTF();
+                    dis.readDouble();
+                    dis.readInt();
+                    linesAmount++;
                 } catch (EOFException e) {
-                    break;  // Koniec pliku
+                    break;
                 }
             }
 
-            if (!znaleziono) {
-                System.out.println("Nie znaleziono towaru o nazwie " + nazwaTowaru);
-                return 0;
+            // Resetowanie pozycji wskaźnika w strumieniu
+            dis.close();
+            DataInputStream dis2 = new DataInputStream(new FileInputStream(nazwaPliku));
+
+            String[] towar = new String[linesAmount];
+            double[] cena = new double[linesAmount];
+            int[] sztuki = new int[linesAmount];
+
+            // Wczytywanie danych do tablic
+            for (int i = 0; i < linesAmount; i++) {
+                towar[i] = dis2.readUTF();
+                cena[i] = dis2.readDouble();
+                sztuki[i] = dis2.readInt();
+
+                // Zastosowanie rabatu dla danego towaru
+                if (towar[i].equals(nazwaTowaru) && sztuki[i] >= 100) {
+                    sztuki[i] = (int) (sztuki[i] * rabat);
+                    liczbaSztuk = sztuki[i];
+                }
+
+                // Zapis przetworzonych danych do nowego pliku
+                dos.writeUTF(towar[i]);
+                dos.writeDouble(cena[i]);
+                dos.writeInt(sztuki[i]);
             }
+
+            dis2.close();
+            dos.close();
 
         } catch (IOException e) {
             e.printStackTrace();
         }
 
+        // Zamiana plików po zakończeniu przetwarzania
         File staryPlik = new File(nazwaPliku);
         File nowyPlik = new File("temp.txt");
         if (staryPlik.delete() && nowyPlik.renameTo(staryPlik)) {
@@ -58,6 +68,7 @@ public class Main {
 
         return liczbaSztuk;
     }
+
     public static void main(String[] args) {
         String nazwaPliku = "dane.txt";
         String nazwaTowaru = "Koszulka";
@@ -65,4 +76,3 @@ public class Main {
         System.out.println("Liczba sztuk towaru " + nazwaTowaru + " po rabacie: " + liczbaSztuk);
     }
 }
-
