6d30091b3b82470363c66681b29e3f8d67db161d
Marcin <knucias123@gmail.com>
2024-01-18 22:03:47 +0100

    Ended task

---
+++ src/PlikiOSwobodnymDostępie/Main.java
@@ -0,0 +1,32 @@
+package PlikiOSwobodnymDostępie;
+
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try (RandomAccessFile file = new RandomAccessFile("dane_domow.txt", "rw")) {
+
+            long liczbaRekordow = file.length() / (4 * 2 + 8 + 4); // String ma 2 bajty na jeden znak
+
+            for (int i = 0; i < liczbaRekordow; i++) {
+                file.seek(i * (4 * 2 + 8 + 4));
+
+                String nazwaDomu = file.readUTF();
+                int liczbaKondygnacji = file.readInt();
+                double cena = file.readDouble();
+                String lokalizacja = file.readUTF();
+
+                if (liczbaKondygnacji == 1 && lokalizacja.equals("wies")) {
+                    cena *= 0.9;
+                    file.seek(file.getFilePointer() - 8);
+                    file.writeDouble(cena);
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
